cbe97fb37989b5553ac9da365eb713da9f51838e
Ryan <stander.ryan@gmail.com>
2023-01-05 22:43:24 +0100

    Rider upgrades

---
+++ Assignment2_chatbox/starting_code/client/Assets/Scripts/TcpChatClient.cs
@@ -0,0 +1,69 @@
+﻿using shared;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+/**
+ * Assignment 2 - Starting project.
+ * 
+ * @author J.C. Wichman
+ * @editor Ryan Stander
+ */
+public class TcpChatClient : MonoBehaviour
+{
+    [SerializeField] private PanelWrapper panelWrapper = null;
+    [SerializeField] private string hostname = "localhost";
+    [SerializeField] private int port = 55555;
+
+    private TcpClient client;
+
+    private void Start()
+    {
+        panelWrapper.OnChatTextEntered += OnTextEntered;
+        ConnectToServer();
+    }
+
+    private void ConnectToServer()
+    {
+        try
+        {
+			client = new TcpClient();
+            client.Connect(hostname, port);
+            panelWrapper.ClearOutput();
+            panelWrapper.AddOutput("Connected to server.");
+        }
+        catch (Exception e)
+        {
+            panelWrapper.AddOutput("Could not connect to server:");
+            panelWrapper.AddOutput(e.Message);
+        }
+    }
+
+    private void OnTextEntered(string pInput)
+    {
+        if (string.IsNullOrEmpty(pInput)) return;
+
+        panelWrapper.ClearInput();
+
+		try 
+        {
+			//echo client - send one, expect one (hint: that is not how a chat works ...)
+			var outBytes = Encoding.UTF8.GetBytes(pInput);
+			StreamUtil.Write(client.GetStream(), outBytes);
+
+			var inBytes = StreamUtil.Read(client.GetStream());
+            var inString = Encoding.UTF8.GetString(inBytes);
+            panelWrapper.AddOutput(inString);
+		} 
+        catch (Exception e) 
+        {
+            panelWrapper.AddOutput(e.Message);
+			//for quicker testing, we reconnect if something goes wrong.
+			client.Close();
+			ConnectToServer();
+		}
+    }
+
+}
+
--- Assignment2_chatbox/starting_code/server/TCPServerSample.cs
+++ Assignment2_chatbox/starting_code/server/TCPServerSample.cs
@@ -1,48 +1,51 @@
 ﻿using System;
-using System.Net.Sockets;
-using System.Net;
 using System.Collections.Generic;
-using shared;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading;
+using shared;
 
-class TCPServerSample
+namespace server
 {
-	/**
+	class TcpServerSample
+	{
+		/**
 	 * This class implements a simple concurrent TCP Echo server.
 	 * Read carefully through the comments below.
 	 */
-	public static void Main (string[] args)
-	{
-		Console.WriteLine("Server started on port 55555");
-
-		TcpListener listener = new TcpListener (IPAddress.Any, 55555);
-		listener.Start ();
-
-		List<TcpClient> clients = new List<TcpClient>();
-
-		while (true)
+		public static void Main (string[] args)
 		{
-			//First big change with respect to example 001
-			//We no longer block waiting for a client to connect, but we only block if we know
-			//a client is actually waiting (in other words, we will not block)
-			//In order to serve multiple clients, we add that client to a list
-			while (listener.Pending()) { 
-				clients.Add(listener.AcceptTcpClient());
-				Console.WriteLine("Accepted new client.");
-			}
+			Console.WriteLine("Server started on port 55555");
 
-			//Second big change, instead of blocking on one client, 
-			//we now process all clients IF they have data available
-			foreach (TcpClient client in clients)
+			var listener = new TcpListener (IPAddress.Any, 55555);
+			listener.Start ();
+
+			var clients = new List<TcpClient>();
+
+			while (true)
 			{
-				if (client.Available == 0) continue;
-				NetworkStream stream = client.GetStream();
-				StreamUtil.Write(stream, StreamUtil.Read(stream));
-			}
+				//First big change with respect to example 001
+				//We no longer block waiting for a client to connect, but we only block if we know
+				//a client is actually waiting (in other words, we will not block)
+				//In order to serve multiple clients, we add that client to a list
+				while (listener.Pending()) { 
+					clients.Add(listener.AcceptTcpClient());
+					Console.WriteLine("Accepted new client.");
+				}
 
-			//Although technically not required, now that we are no longer blocking, 
-			//it is good to cut your CPU some slack
-			Thread.Sleep(100);
+				//Second big change, instead of blocking on one client, 
+				//we now process all clients IF they have data available
+				foreach (var client in clients)
+				{
+					if (client.Available == 0) continue;
+					var stream = client.GetStream();
+					StreamUtil.Write(stream, StreamUtil.Read(stream));
+				}
+
+				//Although technically not required, now that we are no longer blocking, 
+				//it is good to cut your CPU some slack
+				Thread.Sleep(100);
+			}
 		}
 	}
 }
